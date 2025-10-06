c564c50f4fb5ba26bd4baf1373f0ad09227c6dcd
PEDRO HENRIQUE OLIVEIRA DA SILVA <25132766@CFP127ALUNO.LOCAL>
2025-10-06 16:55:00 -0300

    Ja estou conseguindo utilizar a localização atual do cliente

--- app.js
+++ app.js
@@ -1,44 +1,66 @@
 'use strict'
 
-const API_KEY = '7db57ac09fad4f669e4170726252909'
+const API_KEY = '8199697d6dfc44d2b7a161407250610'
 const URL_BASE = 'http://api.weatherapi.com/v1'
 
 const urlParams = new URLSearchParams()
-
 const btnLoc = document.getElementById('local')
-btnLoc.addEventListener('click',pegarLocalizacao) 
+btnLoc.addEventListener('click',pegarLocalizacao)
 
-function pegarLocalizacao()
-{
+
+//função que pega a localização atual e envia como parametro na função armazenarLocalizacao
+function pegarLocalizacao(){
     if(navigator.geolocation){
        navigator.geolocation.getCurrentPosition(armazenarLocalizacao)
-       
+
     }  
 }
 
+//função que armazena a latitude e longitudo e concatena e envia como parametro da função buscarClimaAtual
 function armazenarLocalizacao(position){
-
     let latitude = position.coords.latitude
     let longitude = position.coords.longitude
 
     let urlParamLongitudeLatitude = `${latitude},${longitude}`
 
-    buscarClimaAtual(urlParamLongitudeLatitude)
-
+  
+   mostrarDadosLocAtual(urlParamLongitudeLatitude)
 
 }
 
-async function buscarClimaAtual (localizacao) {
+async function buscarClimaLocAtual (localizacao) {
     urlParams.append('q', localizacao)
     urlParams.append('key', API_KEY)
 
     const urlBase = `${URL_BASE}/current.json`
-    const fetchUrl = `${urlBase}?${urlParams.toString()}`
+    const fetchUrl = `${urlBase}?aqi=yes&${urlParams.toString()}`
     const response = await fetch(fetchUrl)
     const dados = await response.json()
+    return dados
 }
 
-// buscarClimaAtual('London')
+async function mostrarDadosLocAtual (localizacao){
 
+    const loc = await buscarClimaLocAtual(localizacao)
+    const cardTemp = document.getElementById('card')
 
+    let icon = document.createElement('img')
+    const cardMinMax = document.getElementById('card-min-max')
+    const cardQualidade = document.getElementById('card-qualidade')
+    let temperatura = document.createElement('h3')
+    let cidade = document.createElement('h2')
+    let min = document.createElement('h2')
+    let max = document.createElement('h2')
+    let qualidade = document.createElement('h3')
+    cidade.textContent = loc['location'].name
+    card.appendChild(cidade)
 
+    temperatura.textContent = loc['current'].temp_c
+    cardTemp.appendChild(temperatura)
+
+    icon.src = loc['current'].condition.icon
+    cardTemp.appendChild(icon)    
+    qualidade.textContent = loc.current.air_quality["us-epa-index"]
+
+    cardQualidade.appendChild(qualidade)
+}
